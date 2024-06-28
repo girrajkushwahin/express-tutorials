6cff6b249028495e8348c6b667485fc833382b68
Girraj Kushwah <tgirrajindia@gmail.com>
2024-06-28 23:10:00 +0530

    edited app1.js file

--- expresslearn/src/app1.js
+++ expresslearn/src/app1.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const app=express();
+const app = express();
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('hello from express server');
 })
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
     res.send('hello from about page');
 })
 
-app.listen(8000,()=>{
+app.listen(8000, () => {
     console.log('listening to port 8000');
 })
